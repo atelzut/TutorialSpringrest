66e10c3b263d85c344ef93b93120d317d4e29411
l.siracusa@reply.it <SirLeoReply1!>
2022-08-12 12:56:40 +0200

    added postgres db connection

--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
-based on https://spring.io/guides/tutorials/rest/ tutorial
+based on 
+
+https://spring.io/guides/tutorials/rest/ tutorial
+
+https://www.codejava.net/frameworks/spring-boot/connect-to-postgresql-database-examples
--- demo/src/main/java/com/example/demo/LoadDatabase.java
+++ demo/src/main/java/com/example/demo/LoadDatabase.java
@@ -12,25 +12,25 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-    @Bean
-    CommandLineRunner initDatabase (EmployeeRepository employeeRepository, OrderRepository orderRepository){
-
-        return args -> {
-            employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar"));
-            employeeRepository.save(new Employee("Frodo", "Baggins", "thief"));
-
-            employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
-
-            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
-            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
-
-            orderRepository.findAll().forEach(order -> {
-                log.info("Preloaded " + order);
-            });
-        };
-    }
+//    @Bean
+//    CommandLineRunner initDatabase (EmployeeRepository employeeRepository, OrderRepository orderRepository){
+//
+//        return args -> {
+//            employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar"));
+//            employeeRepository.save(new Employee("Frodo", "Baggins", "thief"));
+//
+//            employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+//
+//            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+//            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+//
+//            orderRepository.findAll().forEach(order -> {
+//                log.info("Preloaded " + order);
+//            });
+//        };
+//    }
 }
